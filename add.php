224507234b88e62301f08074e408f91e12ea02ee
Robert Dudasik <robertadudasik@lewisu.edu>
2017-03-25 23:20:47 -0500

    Big Upload Many Changes

--- add.php
+++ add.php
@@ -7,15 +7,18 @@
 	$username = "lewishthon10";
 	$password = "hack121";
 	$db='hackdb10';
+	$email=$_POST['email'];
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password, $db);
 	// Check connection
 	if (!$conn) {
 	    die("Connection failed: " . mysqli_connect_error());
 	}
-	$newDate = date("Y-d-m", strtotime($originalDate));
+	$newDate = date("Y-m-d", strtotime($originalDate));
 	
-	$query= "INSERT INTO `destinations` (`id`, `location`, `date`, `description`, `rating`) VALUES (NULL, '$location', '$newDate','$description',$rating);";
+	$query= "INSERT INTO `destinations` (`id`, `location`, `date`, 
+`description`, `rating`,`email`) VALUES (NULL, '$location', 
+'$newDate','$description',$rating,'$email');";
 	//Attempt to insert data into data base. Output error if any
 	//If Success
 	if (mysqli_query($conn, $query)) {
---
+++ history.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<title>Travel History</title>
+	</head>
+<body>
+
+<?php
+	
+@$db=new mysqli('localhost','lewishthon10', 'hack121','hackdb10');
+
+	if($db->connect_error){
+		die('Connect Error' . $db->connect_errno . ': ' .$db->connect_error);
+	}
+	$email= $_POST['email'];
+
+	$query = "select location,date,description,rating,email from destinations where email='$email' order by rating";
+
+	$result=$db->query($query);
+	$num_results = $result->num_rows;
+	echo "<p>$num_results destinations found!</p>";
+	echo "<table><tr>";
+	echo "<th>Location</th>";
+	echo "<th>Description</th>";
+	echo "<th>Date</th>";
+	echo "<th>Rating</th>";
+	echo "<th>Email</th>";
+	echo "</tr>";
+	
+	
+	for($i=0; $i<$num_results;$i++){
+		echo "<tr>";
+		$row=$result->fetch_assoc();
+		echo "<td>".htmlspecialchars(stripslashes($row['location']))."</td>";
+		echo "<td>".htmlspecialchars(stripslashes($row['date']))."</td>";
+		echo "<td>".htmlspecialchars(stripslashes($row['description']))."</td>";
+		echo "<td>".htmlspecialchars(stripslashes($row['rating']))."</td>";
+		echo "<td>".htmlspecialchars(stripslashes($row['email']))."</td>";
+		echo"</tr>";
+	}
+	echo "</table>";
+	
+	
+	$result->free();
+	$db->close();
+
+?>
+
+</body>
+</html>
